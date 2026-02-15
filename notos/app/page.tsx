4f130a2169f24de2b3ad8dd7b19819de1f075bab
Tris Wong <tristonwongis@gmail.com>
2026-02-15 03:49:10 -0500

    a lil balloon

--- notos/app/page.tsx
+++ notos/app/page.tsx
@@ -6,36 +6,42 @@ import { loginAccount, checkSupabaseConnection } from "../app/lib/supabase";
 import { Header } from "../components/Header";
 import ShowButtonHover from "@/components/ButtonHover";
 import Task from "../components/Task";
+import { div } from "framer-motion/client";
 
-export default async function Page() {
-  const session = await auth0.getSession();
-  if (!session) {
-    return (
-      <>
-        <Header email={null} perfName={null} />
-        {/* Redirects to Auth0 to sign up */}
-        <a href="/auth/login?screen_hint=signup">Signup</a>
-        <br />
-        {/* Redirects to Auth0 to log in */}
-        <a href="/auth/login">Login</a>
-      </>
-    );
-  }
+export default function Page() {
+  // const session = await auth0.getSession();
+  // if (!session) {
+  //   return (
+  //     <>
+  //       <Header email={null} perfName={null} />
+  //       {/* Redirects to Auth0 to sign up */}
+  //       <a href="/auth/login?screen_hint=signup">Signup</a>
+  //       <br />
+  //       {/* Redirects to Auth0 to log in */}
+  //       <a href="/auth/login">Login</a>
+  //     </>
+  //   );
+  // }
 
+  // return (
+  //   <div>
+  //     <div>
+  //       <Header email={session.user.email} perfName={session.user.name} />
+  //       <p>Logged in as {session.user.email}</p>
+
+  //       {/* Display user info (name, email, etc.) */}
+  //       <h1>User Profile</h1>
+  //       <pre>{loginAccount(session.user.email)}</pre>
+
+  //       {/* Ends the session and redirects to Auth0 to log out */}
+  //       <a href="/auth/logout">Logout</a>
+  //     </div>
+  //     <div></div>
+  //   </div>
+  // );
   return (
     <div>
-      <div>
-        <Header email={session.user.email} perfName={session.user.name} />
-        <p>Logged in as {session.user.email}</p>
-
-        {/* Display user info (name, email, etc.) */}
-        <h1>User Profile</h1>
-        <pre>{loginAccount(session.user.email)}</pre>
-
-        {/* Ends the session and redirects to Auth0 to log out */}
-        <a href="/auth/logout">Logout</a>
-      </div>
-      <div></div>
+      <Task color="red" />
     </div>
-  );
+  )
 }
